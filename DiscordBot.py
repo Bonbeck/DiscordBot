0075ec145b1806c1a39a9bcd2c34c48328f615aa
Bonbeck <matheus8bonvechio@hotmail.com>
2021-11-09 14:57:14 -0300

    bot começou a responder

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,4 +1,4 @@
-import discord, json
+import discord, json, random
 
 with open("token.json") as jToken:
     token = json.load(jToken)
@@ -10,5 +10,22 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
 
+        if message.author == client.user:
+            return
+
+        brooklyn_99_quotes = [
+            'I\'m the human form of the 💯 emoji.',
+            'Bingpot!',
+            (
+                'Cool. Cool cool cool cool cool cool cool, '
+                'no doubt no doubt no doubt no doubt.'
+            ),
+        ]
+
+        if message.content == '99!':
+            response = random.choice(brooklyn_99_quotes)
+            await message.channel.send(response)
+
 client = MyClient()
+
 client.run(token)
